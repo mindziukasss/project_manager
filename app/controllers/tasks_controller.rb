08e87044d48b2b93ce846b3ec9b9d5ecc785cf2f
mindziukasss <mindziukasss@gmail.com>
2016-10-14 16:20:56 +0300

    Mark as  complete ability for task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
 	before_action :set_project
-
+	before_action :set_task, except: [:create]
 	def create
 		@task = @project.tasks.create(task_params)
 
@@ -9,7 +9,6 @@ class TasksController < ApplicationController
 	end
 
 	def destroy
-		@task = @project.tasks.find(params[:id])
 		if @task.destroy
 			flash[:success] = "Task was delete."
 		else
@@ -18,10 +17,20 @@ class TasksController < ApplicationController
 		redirect_to @project
 	end
 
+	def complete
+		@task.update_attribute(:completed_at, Time.now)
+		redirect_to @project, notice: "Task Completed"
+	end
+
 	private
 
 	def set_project
 		@project = Project.find(params[:project_id])
+		
+	end
+
+	def set_task
+		@task = @project.tasks.find(params[:id])
 		
 	end
 
